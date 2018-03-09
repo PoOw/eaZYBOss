8d11139159f4e3af0368a3f196ed878fff315c48
Pauline Huguenel <pauline.huguenel@ensimag.grenoble-inp.fr>
2018-03-09 11:04:34 +0100

    Création boutons + test initial

--- app/src/main/java/fr/ensimag/eazyboss/MainActivity.java
+++ app/src/main/java/fr/ensimag/eazyboss/MainActivity.java
@@ -2,12 +2,37 @@ package fr.ensimag.eazyboss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button scanEtu;
+    private Button scanProf;
+    private Button scanCarte;
+
+    private View.OnClickListener scanner = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // TODO : lancer le scan
+            CharSequence text = "Hello toast!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+            toast.show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scanEtu = findViewById(R.id.button_scan_etu);
+        scanProf = findViewById(R.id.button_scan_prof);
+        scanCarte = findViewById(R.id.button_scan_carte);
+
+        scanEtu.setOnClickListener(scanner);
+        scanProf.setOnClickListener(scanner);
+        scanCarte.setOnClickListener(scanner);
     }
 }
