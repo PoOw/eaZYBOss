3c8443ca4afecdc6cba47ad6497b372839a71dcb
Loic Poncet <Loic.Poncet@grenoble-inp.org>
2018-03-12 22:56:41 +0100

    Modif du gitignore et ajout du scan de codes barres

--- app/src/main/java/fr/ensimag/eazyboss/MainActivity.java
+++ app/src/main/java/fr/ensimag/eazyboss/MainActivity.java
@@ -1,10 +1,13 @@
 package fr.ensimag.eazyboss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import com.google.android.gms.vision.barcode.Barcode;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button scanEtu;
@@ -15,13 +18,26 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             // TODO : lancer le scan
-            CharSequence text = "Hello toast!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-            toast.show();
+            Intent intent = new Intent(getApplicationContext(), ScanActivity.class);
+            startActivityForResult(intent, 0); // We launch the scan
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    // We get the barcode that the scan activity sent
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    scanEtu.setText(barcode.displayValue);
+                } else {
+                    scanEtu.setText("Aucun barcode detecté !");
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
