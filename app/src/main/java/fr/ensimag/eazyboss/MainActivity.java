7eb098aeb8dbaa5d3b4327c821ec4f0831019527
Pauline Huguenel <pauline.huguenel@ensimag.grenoble-inp.fr>
2018-05-24 15:26:19 +0200

    Chgt bouton radio contre liste

--- app/src/main/java/fr/ensimag/eazyboss/MainActivity.java
+++ app/src/main/java/fr/ensimag/eazyboss/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -160,6 +162,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        Spinner spinner = (Spinner) findViewById(R.id.duration_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.duration_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
     }
 
